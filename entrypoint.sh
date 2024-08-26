c42fc4a139a51585783955040864de1c4453f9c4
Its4Nik <info@itsnik.de>
2024-08-26 16:03:51 +0200

    Updated Docker entrypoint and added example docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+API_URL="${API_URL//\"}"
+DEFAULT_THEME="${DEFAULT_THEME//\"}"
+SECRET="${SECRET//\"}"
+LOGO_SIZE="${LOGO_SIZE//\"}"
+DM_LOGO_COLOR="${DM_LOGO_COLOR//\"}"
+LM_LOGO_COLOR="${LM_LOGO_COLOR//\"}"
+
 if [[ -z "${DEFAULT_THEME}" ]]; then
     DEFAULT_THEME="dracula"
 fi
@@ -11,17 +18,38 @@ else
     touch touch /app/build/config.json
 fi
 
-echo "============ DockStat ============"
-echo "API_URL:       ${API_URL}"
-echo "DEFAULT_THEME: ${DEFAULT_THEME}"
-echo "SECRET:        ${SECRET}"
-echo "============ DockStat ============"
+case "$LOGO_SIZE" in
+    s|S)
+        LOGO_SIZE="s"
+        ;;
+    m|M)
+        LOGO_SIZE="m"
+        ;;
+    l|L)
+        LOGO_SIZE="l"
+        ;;
+    xl|XL|Xl|xL)
+        LOGO_SIZE="xl"
+        ;;
+esac
+
+echo "================== DockStat =================="
+echo "API_URL               : ${API_URL}"
+echo "DEFAULT_THEME         : ${DEFAULT_THEME}"
+echo "SECRET                : ${SECRET}"
+echo "LOGO SIZE             : ${LOGO_SIZE}"
+echo "DARK MODE LOGO COLOR  : ${DM_LOGO_COLOR}"
+echo "LIGHT MODE LOGO COLOR : ${LM_LOGO_COLOR}"
+echo "================== DockStat =================="
 
 echo "
 {
-    \"API_URL\": ${API_URL},
-    \"DEFAULT_THEME\": ${DEFAULT_THEME},
-    \"SECRET\": ${SECRET}
+    \"API_URL\": \"${API_URL}\",
+    \"DEFAULT_THEME\": \"${DEFAULT_THEME}\",
+    \"SECRET\": \"${SECRET}\",
+    \"LOGO_SIZE\": \"${LOGO_SIZE}\",
+    \"DARK_MODE_LOGO_COLOR\": \"$(echo "${DM_LOGO_COLOR}" | tr -d '#')\",
+    \"LIGHT_MODE_LOGO_COLOR\": \"$(echo "${LM_LOGO_COLOR}" | tr -d '#')\"
 }
 " > /app/build/config.json
 
